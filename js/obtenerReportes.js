217d47f857023589f65fbfdbcf44834acfab9523
Rafael Barajas <rafael.barajas@ideen.mx>
2021-01-12 12:56:28 -0600

    Adds class to cell on table

--- js/obtenerReportes.js
+++ js/obtenerReportes.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 
                     //Se crean las celdas respectivas a los datos necesarios
                     var cell1 = row.insertCell(0);
+                    cell1.addClass('id_reporte');
                     var cell2 = row.insertCell(1);
                     var cell3 = row.insertCell(2);
                     var cell4 = row.insertCell(3);
